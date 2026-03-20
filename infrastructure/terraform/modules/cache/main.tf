c489e40449434b5f93b90d9bccc9b0ebec2865bb
Stephen Omondi <omondi13@gmail.com>
2026-03-20 23:38:14 +0300

    feat: Add Terraform modules for Azure Redis Cache and Azure Function App compute resources.

--- infrastructure/terraform/modules/cache/main.tf
+++ infrastructure/terraform/modules/cache/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_redis_cache" "main" {
   capacity            = 0
   family              = "C"
   sku_name            = "Basic"
-  enable_non_ssl_port = false
+  non_ssl_port_enabled = false
   minimum_tls_version = "1.2"
 
   tags = {
