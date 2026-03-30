c2cd00c8531e70627124aa4874986c14eafe2f6a
Stephen Omondi <omondi13@gmail.com>
2026-03-30 17:07:46 +0300

    feat: initialize dependency injection and database configuration for all Azure Function apps

--- src/FunctionApps/PropertyOwnerService/Program.cs
+++ src/FunctionApps/PropertyOwnerService/Program.cs
@@ -38,18 +38,4 @@ var host = new HostBuilder()
     })
     .Build();
 
-using (var scope = host.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<StayHereDbContext>();
-    try
-    {
-        await context.Database.MigrateAsync();
-    }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider.GetRequiredService<Microsoft.Extensions.Logging.ILoggerFactory>().CreateLogger("Startup");
-        logger.LogError(ex, "Failed to apply migrations on startup.");
-    }
-}
-
 await host.RunAsync();
--- src/FunctionApps/StaticDataService/Program.cs
+++ src/FunctionApps/StaticDataService/Program.cs
@@ -33,20 +33,4 @@ var host = new HostBuilder()
     })
     .Build();
 
-// Apply migrations on startup
-using (var scope = host.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<StayHereDbContext>();
-    try
-    {
-        await context.Database.MigrateAsync();
-    }
-    catch (Exception ex)
-    {
-        // Log error but allow host to start so we can debug/sync triggers
-        var logger = scope.ServiceProvider.GetRequiredService<Microsoft.Extensions.Logging.ILoggerFactory>().CreateLogger("Startup");
-        logger.LogError(ex, "Failed to apply migrations on startup.");
-    }
-}
-
 await host.RunAsync();
