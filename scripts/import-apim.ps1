b16cc0cce87ef329706bb90699c6d3c2bb0351eb
Stephen Omondi <omondi13@gmail.com>
2026-05-04 20:19:23 +0300

    feat: add PowerShell script to automate Terraform APIM operation imports

--- scripts/import-apim.ps1
+++ scripts/import-apim.ps1
@@ -10,7 +10,21 @@ $APIM = "apim-$Env-$Suffix"
 function Import-Operation {
     param($ResourceName, $ApiName, $OperationId)
     Write-Host "Importing $ResourceName ($OperationId)..."
-    terraform import "module.apim.azurerm_api_management_api_operation.$ResourceName" "/subscriptions/039755a5-67c2-48a5-9304-448c909618f6/resourceGroups/$RG/providers/Microsoft.ApiManagement/service/$APIM/apis/$ApiName/operations/$OperationId"
+    
+    # Pass variables from environment (Azure DevOps) to prevent interactive prompts
+    terraform import -input=false `
+        -var="mongodb_atlas_public_key=$($env:MONGODB_ATLAS_PUBLIC_KEY)" `
+        -var="mongodb_atlas_private_key=$($env:MONGODB_ATLAS_PRIVATE_KEY)" `
+        -var="mongodb_atlas_org_id=$($env:MONGODB_ATLAS_ORG_ID)" `
+        -var="skip_auth=$($env:SKIP_AUTH)" `
+        -var="openrouter_api_key=$($env:OPENROUTER_API_KEY)" `
+        -var="openrouter_model=$($env:OPENROUTER_MODEL)" `
+        -var="openrouter_embedding_model=$($env:OPENROUTER_EMBEDDING_MODEL)" `
+        -var="onfon_client_id=$($env:ONFON_CLIENT_ID)" `
+        -var="onfon_api_key=$($env:ONFON_API_KEY)" `
+        -var="onfon_base_url=$($env:ONFON_BASE_URL)" `
+        -var="onfon_sender_id=$($env:ONFON_SENDER_ID)" `
+        "module.apim.azurerm_api_management_api_operation.$ResourceName" "/subscriptions/039755a5-67c2-48a5-9304-448c909618f6/resourceGroups/$RG/providers/Microsoft.ApiManagement/service/$APIM/apis/$ApiName/operations/$OperationId"
 }
 
 # --- Auth API Operations ---
